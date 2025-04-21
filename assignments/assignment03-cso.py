604339077659b2c5fa4c1fe867fe0078b03a4a51
chavvy <samedo1999@gmail.com>
2025-04-21 03:19:44 +0100

    assignment 3 upload

---
+++ assignments/assignment03-cso.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
+
+def get_cso_data():
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        with open("cso.json", "w") as data:
+            data.write(response.text)
+        print("Data has been saved to cso.json.")
+    else:
+        print(f"Error retrieving data. Status code: {response.status_code}")
+
+if __name__ == "__main__":
+    get_cso_data()
